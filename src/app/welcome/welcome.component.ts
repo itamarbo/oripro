626a466671aa3c144a23af282ac0f5c1a60f3b44
itamarbo <itamar960@gmail.com>
2020-07-21 17:01:09 +0300

    upload service

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FileUploaderService } from './../file-uploader.service';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  fileObj: File;
+  fileUrl: string;
+  errorMsg: boolean;
+  title = 's3-file-uploader-app';
+
+  constructor(private fileUploaderService:FileUploaderService) {
+    this.errorMsg = false;
+   }
 
   ngOnInit(): void {
   }
 
+  onFilePicked(event: Event): void {
+
+    this.errorMsg = false;
+    console.log(event);
+    const FILE = (event.target as HTMLInputElement).files[0];
+    this.fileObj = FILE;
+    console.log(this.fileObj);
+  }
+  
+  onFileUpload() {
+
+    if (!this.fileObj) {
+      this.errorMsg = true;
+      return
+    }
+    
+    const fileForm = new FormData();
+    fileForm.append('file', this.fileObj);
+    this.fileUploaderService.fileUpload(fileForm).subscribe(res => {
+      console.log(res);
+      this.fileUrl = res['image'];
+    });
+  }
+
 }
+
